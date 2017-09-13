84f9078cda936e0f8dde23a30e4559266d568c44
Seije <niels.gaastra@hotmail.com>
2017-09-13 20:21:11 +0200

    added getTableContour which finds and returns the table counter

--- track_ball.py
+++ track_ball.py
@@ -2,8 +2,6 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-import Indexer
-
 # Load image in BGR (params: image_path)
 def loadImage(image_path):
     return cv2.imread(image_path, 1)
@@ -16,12 +14,15 @@ def toHSV(image):
 def getDist(x1, y1, x2, y2):
     return np.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1))
 
-# Tries to mask table in HSV image (params: image, search_range = 45, shadow_intensity = 20)
-def getTableMask(image, search_range = 45, shadow_intensity = 20):
+# Tries to mask table in HSV image (params: image, search_range = 45, shadow_intensity = 20, blur = 3)
+def getTableMask(image, search_range = 45, shadow_intensity = 20, blur = 3):
     # Get most common color in 'image' with a certain range
-    min_color, max_color = getTableColor(image, search_range, shadow_intensity)
-    return cv2.inRange(image, min_color, max_color)
-
+	min_color, max_color = getTableColor(image, search_range, shadow_intensity)
+	mask = cv2.inRange(image, min_color, max_color)
+	clean_mask = cv2.medianBlur(mask, blur)
+	
+	return clean_mask
+	
 # Guess table color with high succes rate (params: image, search_range)
 def getTableColor(hsv, search_range, shadow_intensity):
     # Get histogram from image's hue channel and get max value
@@ -42,9 +43,16 @@ def getTableColor(hsv, search_range, shadow_intensity):
 
     return lower_color, upper_color
 
-img = loadImage('pooltable3.jpg')
+def getTableContour(table_mask):
+	_, contours, _ = cv2.findContours(table_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+	table_contour = sorted(contours, key=cv2.contourArea, reverse=True)[0]
+	return table_contour
+	
+img = loadImage('pooltable.png')
 hsv = toHSV(img)
-mask = getTableMask(hsv)
+mask = getTableMask(hsv, blur=7)
+table_contour = getTableContour(mask)
+cv2.drawContours(img, [table_contour], -1, (0,255,0), 3)
 
 cv2.imshow('image', img)
 cv2.imshow('mask', mask)
