f37b549094cb97d3311b1fb8cea5379f1b4d321a
NGaastra <ntgaastr@student.avans.nl>
2017-09-19 13:35:34 +0200

    This kind of works, commit to start at new solution

--- track_ball.py
+++ track_ball.py
@@ -6,6 +6,9 @@ import imutils
 # Load image in BGR (params: image_path)
 # params: image_path
 # return: inputs derived image object
+from pip.utils import dist_in_site_packages
+
+
 def loadImage(image_path):
     return cv2.imread(image_path, 1)
 
@@ -37,14 +40,29 @@ def getTableMask(image, search_range = 45, shadow_intensity = 20, blur = True):
 
     return mask
 
-def getBallMask(image):
-    ball_mask = getTableMask(image, blur = False)
-    return ball_mask
+def getBallMask(crop, bg):
+    centers = subBackground(bg, crop)
+
+    #centers = cv2.bitwise_and(crop, crop, mask = new_mask)
+    #centers = cv2.cvtColor(centers, cv2.COLOR_BGR2GRAY)
+    _, contours, _ = cv2.findContours(centers, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    print(len(contours))
+    '''
+    # Get distance transform from table mask
+    dist = cv2.distanceTransform(new_mask, cv2.DIST_L2, 5);
+
+    # Threshold distance transform values by 1/4 max value
+    _, centers = cv2.threshold(dist, 0.25 * dist.max(), 255, cv2.THRESH_BINARY)
+
+    # Dilate to expand found circles
+    centers = cv2.dilate(centers, None, iterations=5)
+    '''
+    return centers
 
 # Get table color
 # params: image, search_range
 # return: guessed minimum color and maximum color of inputted image
-def getTableColor(hsv, search_range, shadow_intensity):
+def getTableColor(hsv, search_range = 25, shadow_intensity = 0):
     # Get histogram from image's hue channel and get max value
     hist = cv2.calcHist([hsv], [0], None, [180], [0, 180])
     h_max = np.argmax(hist)
@@ -85,17 +103,18 @@ def getTableCorners(table_contour):
 # Get all balls from table
 # params: table_mask
 # return: gets all balls that can be detected in 'table_mask'
-def getBallsContour(table_mask):
-	_, contours, _ = cv2.findContours(table_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-	balls = []
-	for contour in contours:
-		points = cv2.approxPolyDP(contour, 0.01 * cv2.arcLength(contour, True), True)
-		area = cv2.contourArea(contour)
-		if len(points) > 8 and area > 70:
-			balls.append(contour)
-	print(len(balls))
-	return balls
-
+def getBallsContour(mask, img):
+    mask = np.array(mask, np.uint8)
+    delete = []
+    _, contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    for i, cnt in enumerate(contours):
+        _, contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        for i, cnt in enumerate(contours):
+            x, y, w, h = cv2.boundingRect(cnt)
+            if not (0.95 < w / h < 1.05):
+                delete.append(i)
+    contours  = np.delete(contours , delete)
+    return contours
 # Crop image according to table
 # params: image, table_contour
 # return: image cropped to 'table_contour' size
@@ -104,22 +123,39 @@ def	isolateTable(image, table_contour):
 	print([int(corners[1][1]),int(corners[3][1]), int(corners[1][0]),int(corners[3][0])])
 	# Crop by slicing array in this order: start y, end y, start x, end x
 	cropped = image[int(corners[1][1]):int(corners[3][1]), int(corners[1][0]):int(corners[3][0])]
-	cropped = imutils.resize(cropped, width=500)
+	cropped = imutils.resize(cropped, height=500)
 	return cropped
+
+def drawBalls(balls, img):
+    for cnt in balls:
+        M = cv2.moments(cnt)
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+        cv2.circle(img, (cX, cY), 15, (0, 255, 0), 2)
+
+def subBackground(bg, image):
+    # Get table mask and bg mask
+    table_mask = cv2.bitwise_not(getTableMask(toHSV(image)))
+    bg_mask = cv2.bitwise_not(getTableMask(toHSV(bg)))
+
+    # Subtract background from original image
+    return table_mask - bg_mask
+
 
 def initTable(image):
     img = loadImage(image)
+    bg = loadImage('img\pooltableempty.png')
     hsv = toHSV(img)
     mask = getTableMask(hsv)
     table_contour = getTableContour(mask)
-    balls = getBallsContour(mask)
     cropped = isolateTable(img, table_contour)
-    ball_mask = getBallMask(img)
+    cropped_bg = isolateTable(bg, table_contour)
+    ball_mask = getBallMask(cropped, cropped_bg)
+    #balls = getBallsContour(ball_mask, cropped)
+    #drawBalls(balls, cropped)
     #debug
-    cv2.drawContours(img, balls, -1, (0,255,0), 1)
-    cv2.imshow('image', img)
+    cv2.imshow('image', cropped)
     cv2.imshow('mask', mask)
-    cv2.imshow('cropped', cropped)
     cv2.imshow('balls', ball_mask)
 	
 initTable('img\pooltable.png')
